98124d03be29bfb7c0f99a88542d202b01378199
RYJramefison <hei.juninho.2@gmail.com>
2025-02-25 12:20:24 +0300

    feat: first commit

---
+++ src/main/java/school/hei/eventmanagerdwbackend/EventManagerDwBackendApplication.java
@@ -0,0 +1,13 @@
+package school.hei.eventmanagerdwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerDwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerDwBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/hei/eventmanagerdwbackend/EventManagerDwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.eventmanagerdwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerDwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
