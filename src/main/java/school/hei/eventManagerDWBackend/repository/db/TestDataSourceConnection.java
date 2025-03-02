1a5e03da75acbbb73026ea13beeab3b4287c3deb
Ranto-Creat <hei.ranto.2@gmail.com>
2025-03-02 21:54:50 +0300

    feat: add this file to test data source connection is connected

--- src/main/java/school/hei/eventManagerDWBackend/repository/db/TestDataSourceConnection.java
+++ src/main/java/school/hei/eventManagerDWBackend/repository/db/TestDataSourceConnection.java
@@ -1,4 +1,15 @@
 package school.hei.eventManagerDWBackend.repository.db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TestDataSourceConnection {
+  public static void main(String[] args) {
+    DataSource dataSource = new DataSource();
+    try (Connection con = dataSource.getConnection()) {
+      System.out.println("You have successfully connected to database");
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
