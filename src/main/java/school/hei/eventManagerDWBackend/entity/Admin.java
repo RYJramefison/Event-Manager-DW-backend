0f89a22468d45bed5775a6948b192832d910da58
Ranto-Creat <hei.ranto.2@gmail.com>
2025-03-04 11:19:34 +0300

    fix: reorganise some files

--- src/main/java/school/hei/eventManagerDWBackend/entity/Admin.java
+++ src/main/java/school/hei/eventManagerDWBackend/entity/Admin.java
@@ -4,19 +4,22 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Getter
 @ToString
 @Setter
+public class Admin extends User {
+  private String adminName;
 
-
-public class Admin extends User{
-    private String adminName;
-
-    public Admin(int id, String name, String email, Timestamp password, LocalDateTime registrationDate, String adminName) {
-        super(id, name, email, password, registrationDate);
-        this.adminName = adminName;
-    }
+  public Admin(
+      int id,
+      String name,
+      String email,
+      String password,
+      LocalDateTime registrationDate,
+      String adminName) {
+    super(id, name, email, password, registrationDate);
+    this.adminName = adminName;
+  }
 }
--- src/main/java/school/hei/eventManagerDWBackend/entity/Client.java
+++ src/main/java/school/hei/eventManagerDWBackend/entity/Client.java
@@ -1,11 +1,11 @@
 package school.hei.eventManagerDWBackend.entity;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-public class Client extends User{
+public class Client extends User {
 
-    public Client(int id, String name, String email, Timestamp password, LocalDateTime registrationDate) {
-        super(id, name, email, password, registrationDate);
-    }
+  public Client(
+      int id, String name, String email, String password, LocalDateTime registrationDate) {
+    super(id, name, email, password, registrationDate);
+  }
 }
