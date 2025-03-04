cf35e58c859beb793bdca3585346cf867b7018bd
LALAINARISON Elodys <hei.elodys@gmail.com>
2025-03-04 18:23:51 +0300

    feat: complete the attribute to event class

--- src/main/java/school/hei/eventManagerDWBackend/entity/User.java
+++ src/main/java/school/hei/eventManagerDWBackend/entity/User.java
@@ -1,4 +1,73 @@
 package school.hei.eventManagerDWBackend.entity;
 
+import java.time.LocalDateTime;
+
 public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime registrationDate;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, LocalDateTime registrationDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
 }
