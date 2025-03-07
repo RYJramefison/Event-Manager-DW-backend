6ac66faf4021640e5ecc9290fb091d640007809c
Ranto-Creat <hei.ranto.2@gmail.com>
2025-03-07 21:48:28 +0300

    feat: add the client controller and service

--- src/main/java/school/hei/eventManagerDWBackend/controller/ClientController.java
+++ src/main/java/school/hei/eventManagerDWBackend/controller/ClientController.java
@@ -1,4 +1,48 @@
 package school.hei.eventManagerDWBackend.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.hei.eventManagerDWBackend.entity.Client;
+import school.hei.eventManagerDWBackend.service.ClientService;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/client")
 public class ClientController {
+    private final ClientService adminService;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients(@RequestParam(defaultValue = "0") int page,
+                                                    @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(adminService.findAllClients(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable int id) {
+        Optional<Client> admin = adminService.findClientById(id);
+        return admin.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createClient(@RequestBody Client admin) {
+        adminService.createClient(admin);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateClient(@PathVariable int id, @RequestBody Client admin) {
+        admin.setId(id);
+        adminService.updateClient(admin);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable int id) {
+        adminService.deleteClientById(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/school/hei/eventManagerDWBackend/service/ClientService.java
+++ src/main/java/school/hei/eventManagerDWBackend/service/ClientService.java
@@ -1,4 +1,35 @@
 package school.hei.eventManagerDWBackend.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.eventManagerDWBackend.entity.Client;
+import school.hei.eventManagerDWBackend.repository.dao.ClientDao;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
 public class ClientService {
+  private final ClientDao clientDao;
+
+  public List<Client> findAllClients(int page, int size) {
+    return clientDao.getAll(page, size);
+  }
+
+  public Optional<Client> findClientById(int id) {
+    return clientDao.getById(id);
+  }
+
+  public void deleteClientById(int id) {
+    clientDao.deleteById(id);
+  }
+
+  public void updateClient(Client client) {
+    clientDao.update(client);
+  }
+
+  public void createClient(Client client) {
+    clientDao.create(client);
+  }
 }
