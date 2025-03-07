98613f8d05f14fc4d464d2fe7b3f1cdacad5198c
Ranto-Creat <hei.ranto.2@gmail.com>
2025-03-07 21:25:44 +0300

    feat: add the file service to admin

--- src/main/java/school/hei/eventManagerDWBackend/service/AdminService.java
+++ src/main/java/school/hei/eventManagerDWBackend/service/AdminService.java
@@ -1,4 +1,35 @@
 package school.hei.eventManagerDWBackend.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.eventManagerDWBackend.entity.Admin;
+import school.hei.eventManagerDWBackend.repository.dao.AdminDao;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
 public class AdminService {
+    private final AdminDao adminDao;
+
+    public List<Admin> getAllAdmins(int page, int size) {
+        return adminDao.getAll(page, size);
+    }
+
+    public Optional<Admin> getAdminById(int id) {
+        return adminDao.getById(id);
+    }
+
+    public void createAdmin(Admin admin) {
+        adminDao.create(admin);
+    }
+
+    public void updateAdmin(Admin admin) {
+        adminDao.update(admin);
+    }
+
+    public void deleteAdminById(int id) {
+        adminDao.deleteById(id);
+    }
 }
