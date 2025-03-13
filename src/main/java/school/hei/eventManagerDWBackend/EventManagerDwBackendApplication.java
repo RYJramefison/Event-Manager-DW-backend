31b1e9089d4c4d766e19eb924b5b5fe78ebff2ea
Ranto-Creat <hei.ranto.2@gmail.com>
2025-03-13 12:10:32 +0300

    fix: add scanBasePAckage

--- src/main/java/school/hei/eventManagerDWBackend/EventManagerDwBackendApplication.java
+++ src/main/java/school/hei/eventManagerDWBackend/EventManagerDwBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "school.hei.eventManagerDWBackend")
 @ComponentScan({"school.hei.eventManagerDWBackend.controller","school.hei.eventManagerDWBackend.entity", "school.hei.eventManagerDWBackend.repository.dao","school.hei.eventManagerDWBackend.service"})
 public class EventManagerDwBackendApplication {
 
