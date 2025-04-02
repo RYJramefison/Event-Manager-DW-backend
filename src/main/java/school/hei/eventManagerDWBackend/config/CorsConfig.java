ab26a5a42c1d63a9a655bea97840e339c39b8ab9
Ranto-Creat <hei.ranto.2@gmail.com>
2025-04-02 08:39:23 +0300

    fix: modify cors config

--- src/main/java/school/hei/eventManagerDWBackend/config/CorsConfig.java
+++ src/main/java/school/hei/eventManagerDWBackend/config/CorsConfig.java
@@ -1,7 +1,10 @@
-package school.hei.eventManagerDWBackend.repository;
+package school.hei.eventManagerDWBackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,12 +15,24 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Appliquer CORS aux endpoints API
+                registry.addMapping("*/**") // Appliquer CORS aux endpoints API
                         .allowedOrigins("http://localhost:3000") // Autoriser React en local
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
                         .allowedHeaders("*") // Autoriser tous les headers
                         .allowCredentials(true); // Autoriser l'envoi de cookies/tokens si besoin
             }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
